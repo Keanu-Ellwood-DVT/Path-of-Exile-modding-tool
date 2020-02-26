506ed416a6e25a08d86155ebddb3a4cd3271fa87
vadash <vadash@gmail.com>
2020-02-26 20:32:27 +0300

    Fixed crash

--- mods/aps310_env.py
+++ mods/aps310_env.py
@@ -18,6 +18,4 @@ def execute(filename, backupfiledata, modifyggpk):
     filedata, encoding, bom = modifyggpk.stringcleanup(backupfiledata, "UTF-16-LE")
     filedatamod=re.sub(r'"shadows_enabled": true,', r'"shadows_enabled": false,', filedata)
     filedatamod=re.sub(r'"exp_fog_is_enabled": true,', r'"exp_fog_is_enabled": false,', filedatamod)
-    filedatamod=re.sub(r'"player_environment_ao": "Metadata/Effects/weather_attachments/rain/rain.ao",', r'"player_environment_ao": "",', filedatamod)
     return filedatamod, encoding, bom
-
