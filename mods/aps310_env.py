358ad6a668f25fc93e694fecabc3bf13d97d0b30
vadash <vadash@gmail.com>
2020-03-18 17:28:52 +0300

    Added use_forced_screenspace_shadows

--- mods/aps310_env.py
+++ mods/aps310_env.py
@@ -17,4 +17,5 @@ def execute(filename, backupfiledata, modifyggpk):
     filedata, encoding, bom = modifyggpk.stringcleanup(backupfiledata, "UTF-16-LE")
     filedatamod=re.sub(r'"shadows_enabled": true,', r'"shadows_enabled": false,', filedata)
     filedatamod=re.sub(r'"exp_fog_is_enabled": true,', r'"exp_fog_is_enabled": false,', filedatamod)
+    filedatamod=re.sub(r'"use_forced_screenspace_shadows": true,', r'"use_forced_screenspace_shadows": false,', filedatamod)
     return filedatamod, encoding, bom
