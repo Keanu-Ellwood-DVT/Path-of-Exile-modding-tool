4f59bb8c7f410f88a1e97c4ce011478a72ddd791
vadash <vadash@gmail.com>
2020-03-27 00:11:03 +0300

    Fixed .aoc
    .mat is Broken for now

--- mods/aps310_aoc.py
+++ mods/aps310_aoc.py
@@ -6,23 +6,19 @@ masterfilter_restrict=[
 masterfilter_exclude=[
     ]
 
-# we keep first 4
+# we null this
 condition=[
-   "ClientAnimationController",
-   "SkinMesh",
-   "FixedMesh",
-   "BoneGroups",
-   #"ParticleEffects",
-   #"DecalEvents",
-   #"Lights",
-   #"SoundEvents",
-   #"WindEvents",
-   #"AnimatedRender",
-   #"TrailsEffects",
-   #"ScreenShake",
-   #"SoundParams",
-   #"EffectPack",
-   #"BoneGroupTrail",
+   "ParticleEffects",
+   "DecalEvents",
+   "Lights",
+   "SoundEvents",
+   "WindEvents",
+   "AnimatedRender",
+   "TrailsEffects",
+   "ScreenShake",
+   "SoundParams",
+   "EffectPack",
+   "BoneGroupTrail",
    ]
 
 def execute(filename, backupfiledata, modifyggpk):
@@ -31,6 +27,6 @@ def execute(filename, backupfiledata, modifyggpk):
     mi=re.finditer(r'(\w+)[\t\r\n ]*\{.*?\}[\t\r ]*(\n|$)', filedata, flags=re.DOTALL)
     for mii in mi :
         tagis=mii.group(1)
-        if tagis not in condition :
+        if tagis in condition :
             filedatamod=re.sub(tagis+r'[\t\r\n ]*\{.*?\}[\t\r ]*(\n|$)', tagis+r'\r\n{\r\n}\r\n', filedatamod, flags=re.DOTALL)
     return filedatamod, encoding, bom
