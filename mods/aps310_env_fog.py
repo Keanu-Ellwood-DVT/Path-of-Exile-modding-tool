ba6677eb249d70589a2a4e902b84ca5147ccac5f
vadash <vadash@gmail.com>
2020-04-16 09:35:12 +0300

    Separated fog and shadows

---
+++ mods/aps310_env_fog.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import binascii
+import sys
+import time
+import re
+import os
+
+displaylabel=""
+
+masterfilter_restrict=[
+    ]
+
+masterfilter_exclude=[
+    ]
+
+def execute(filename, backupfiledata, modifyggpk):
+    filedata, encoding, bom = modifyggpk.stringcleanup(backupfiledata, "UTF-16-LE")
+    filedatamod=re.sub(r'"exp_fog_is_enabled": true,', r'"exp_fog_is_enabled": false,', filedatamod)
+    return filedatamod, encoding, bom
